202def35dde83461bd79d879442a7d7f4fe8b120
ndsanja <ndsanja@gmail.com>
2022-12-17 03:27:14 +0700

    create extended fab class component

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@ onMounted(() => {
     class="flex flex-col items-center justify-center space-y-2 min-h-screen w-full text-on-background-light dark:text-on-background-dark bg-background-light dark:bg-background-dark">
     <NuxtLink to="/md/button" class="underline" @click="generateColor('#881337')">goto button</NuxtLink>
     <NuxtLink to="/md/fab" class="underline">goto fab</NuxtLink>
+    <NuxtLink to="/md/extended-fab" class="underline">goto extended fab</NuxtLink>
     <button @click="generateColor('#14b8a6')">theme</button>
   </div>
 </template>
---
+++ pages/md/extended-fab.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Fab } from '~~/md/vue';
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center space-y-4 py-10 bg-background-light dark:bg-background-dark">
+    <div class="flex flex-col items-center justify-center">
+      <h1 class="text-xl mb-2">default</h1>
+      <div class="flex items-center justify-center">
+        <button class="ExtendedFab color-surface icon-start">
+          <span class="ExtendedFab__icon">+</span>
+          <span class="ExtendedFab__label">Extended</span>
+        </button>
+        <button class="ExtendedFab color-primary icon-end">
+          <span class="ExtendedFab__label">Extended</span>
+          <span class="ExtendedFab__icon">+</span>
+        </button>
+        <button class="ExtendedFab color-secondary icon-start">
+          <span class="ExtendedFab__icon">+</span>
+          <span class="ExtendedFab__label">Extended</span>
+        </button>
+        <button class="ExtendedFab color-tertiary icon-end">
+          <span class="ExtendedFab__label">Extended</span>
+          <span class="ExtendedFab__icon">+</span>
+        </button>
+        <button class="ExtendedFab color-error icon-start">
+          <span class="ExtendedFab__icon">+</span>
+          <span class="ExtendedFab__label">Extended</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
